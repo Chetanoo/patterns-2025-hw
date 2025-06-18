87fe5fd78c3ff0193bc66e5427e89fa34eaf033d
KirillGorinDataArt <chetanvse@gmail.com>
2025-06-18 20:54:15 +0400

    default

--- 3/factory/4-method-config.js
+++ 3/factory/4-method-config.js
@@ -16,17 +16,6 @@ class Creator {
   }
 }
 
-// Another approach
-class Creator2 {
-  constructor(Blueprint) {
-    this.Blueprint = Blueprint;
-  }
-
-  factoryMethod(...args) {
-    return new this.Blueprint(...args);
-  }
-}
-
 // Usage
 
 const config = { defaultValue: 'default' };
@@ -37,9 +26,3 @@ const product = creator.factoryMethod('value');
 console.dir(product);
 const product2 = creator.factoryMethod();
 console.dir(product2);
-
-
-const creator2 = new Creator2(Product);
-console.dir(creator2);
-const productV = creator2.factoryMethod('value2');
-console.dir(productV);
