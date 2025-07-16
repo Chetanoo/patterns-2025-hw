d3ded1477f0d293ca742072fa0175d482088d51f
KirillGorinDataArt <chetanvse@gmail.com>
2025-07-16 18:33:15 +0400

    week 5 facade 2-class

---
+++ 5/Facade/2-class.d.ts
@@ -0,0 +1,25 @@
+declare class TimeoutCollection<K, V> {
+  constructor(timeout: number);
+
+  set(key: K, value: V): void;
+
+  get(key: K): V | undefined;
+
+  has(key: K): boolean;
+
+  delete(key: K): void;
+
+  forEach(callback: (value: V, key: K, collection: Map<K, V>) => void): void;
+
+  clear(): void;
+
+  toArray(): IterableIterator<[K, V]>;
+
+  values(): IterableIterator<V>;
+
+  entries(): IterableIterator<[K, V]>;
+
+  keys(): IterableIterator<K>;
+}
+
+export = TimeoutCollection;
---
+++ 5/Facade/2-class.js
@@ -0,0 +1,86 @@
+'use strict';
+
+class TimeoutCollection {
+  constructor(timeout) {
+    this.timeout = timeout;
+    this.collection = new Map();
+    this.timers = new Map();
+  }
+
+  set(key, value) {
+    const timer = this.timers.get(key);
+    if (timer) clearTimeout(timer);
+    const timeout = setTimeout(() => {
+      this.delete(key);
+    }, this.timeout);
+    if (timeout.unref) {
+      timeout.unref();
+    }
+    this.collection.set(key, value);
+    this.timers.set(key, timeout);
+  }
+
+  get(key) {
+    return this.collection.get(key);
+  }
+
+  has(key) {
+    return this.collection.has(key);
+  }
+
+  delete(key) {
+    const timer = this.timers.get(key);
+    if (timer) {
+      clearTimeout(timer);
+      this.collection.delete(key);
+      this.timers.delete(key);
+    }
+  }
+
+  forEach(callback) {
+    this.collection.forEach(callback);
+  }
+
+  clear() {
+    for (const [key, timer] in this.timers) {
+      clearTimeout(timer);
+      this.collection.delete(key);
+      this.timers.delete(key);
+    }
+  }
+
+  toArray() {
+    return this.collection.entries();
+  }
+
+  values() {
+    return this.collection.values();
+  }
+
+  entries() {
+    return this.collection.entries();
+  }
+
+  keys() {
+    return this.collection.keys();
+  }
+}
+
+// Usage
+
+const hash = new TimeoutCollection(1000);
+hash.set('uno', 1);
+console.dir({ array: hash.toArray() });
+
+hash.set('due', 2);
+console.dir({ array: hash.toArray() });
+
+setTimeout(() => {
+  hash.set('tre', 3);
+  console.dir({ array: hash.toArray() });
+
+  setTimeout(() => {
+    hash.set('quattro', 4);
+    console.dir({ array: hash.toArray() });
+  }, 500);
+}, 1500);
